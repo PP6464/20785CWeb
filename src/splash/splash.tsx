32e82407896f20a2faf11997c9f937b3429d0b8a
Panth Patel <panthhp@outlook.com>
2023-05-29 10:20:44 +0100

    setup

---
+++ src/splash/splash.tsx
@@ -0,0 +1,13 @@
+import './splash.css';
+
+function Splash() {
+  return (
+    <div id="loading-container">
+      <h1>2</h1>
+      <img src="/assets/logo.png" alt="GEAR" />
+      <h1>0785C</h1>
+    </div>
+  );
+}
+
+export default Splash;
