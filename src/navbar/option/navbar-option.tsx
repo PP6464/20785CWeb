4bb654d564c59300f4dea28ff1681d2addca0754
Panth Patel <panthhp@outlook.com>
2023-05-30 13:31:38 +0100

    navbar option

---
+++ src/navbar/option/navbar-option.tsx
@@ -0,0 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import './navbar-option.css'
+
+interface NavbarOptionProps {
+    title: string
+    selected: boolean
+    path: string
+    onSelect: () => any
+}
+
+function NavbarOption(props: NavbarOptionProps) {
+    const navigate = useNavigate()
+    
+    function navigateToPath() {
+        navigate(props.path);
+        props.onSelect()
+    }
+    
+    return (
+        <li className="navbar-option" onClick={navigateToPath} data-selected={props.selected.toString()}>
+            <h1>{props.title}</h1>
+        </li>
+    );
+}
+
+export default NavbarOption
