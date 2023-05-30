58f1095cb254b0df863483558d24e8d5e0e1e4bc
Panth Patel <panthhp@outlook.com>
2023-05-30 14:38:23 +0100

    navbar

--- src/navbar/navbar.tsx
+++ src/navbar/navbar.tsx
@@ -1,13 +1,23 @@
 import {Outlet} from "react-router-dom";
-import './navbar.css'
+import './navbar.css';
 import NavbarOption from "./option/navbar-option";
 import Loading from "../loading/loading";
-import {useState} from "react";
-import * as fs from "fs";
+import {useEffect, useState} from "react";
 
 function Navbar() {
     const [index, setIndex] = useState(0)
-    const sponsors = JSON.parse(fs.readFileSync("./data/sponsors.json", "utf-8"))
+    const [sponsors, setSponsors] = useState([])
+
+    function loadSponsors() {
+        fetch("/data/sponsors.json").then(sponsors => {
+            sponsors.json().catch(_ => {
+            }).then(data => {
+                setSponsors(data)
+            })
+        })
+    }
+
+    useEffect(loadSponsors, []);
 
     return (
         <div>
@@ -19,14 +29,16 @@ function Navbar() {
                     <div id="sponsors">
                         {
                             sponsors.map((sponsor: any) => (
-                                    <div key={sponsor.id}>{sponsor.name}</div>
-                                )
-                            )
+                                <a key={sponsor.id} style={{display: "flex", alignItems: "center", color: "white", textDecoration: "none"}} href={sponsor.website} target="_blank" rel="noreferrer">
+                                    <img src={sponsor.photo} style={{height: "10vh", borderRadius: "50%"}} alt=""/>
+                                    <h1 style={{paddingLeft: "1rem", paddingRight: "1rem"}}>{sponsor.name}</h1>
+                                </a>
+                            ))
                         }
                     </div>
-                    <img src="/assets/qe-logo.png" alt="QE Barnet" style={{padding: "1rem", height: "15vh"}}
-                         onClick={() => {
-                         }}/>
+                    <a href="https://www.qebarnet.co.uk" target="_blank" rel="noreferrer">
+                        <img src="/assets/qe-logo.png" alt="QE Barnet" style={{padding: "1rem",height: "15vh"}}/>
+                    </a>
                 </div>
                 <ul id="navbar-ul">
                     <NavbarOption onSelect={() => setIndex(0)} path="/" selected={index === 0} title="Home"/>
