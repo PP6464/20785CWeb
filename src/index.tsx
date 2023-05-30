8e9efd03d323b25b69f31985052bf3e25bfb3b86
Panth Patel <panthhp@outlook.com>
2023-05-30 13:31:07 +0100

    index

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from "./app/app";
 import './index.css';
-import Splash from './splash/splash';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Splash />
+    <App />
   </React.StrictMode>
 );
