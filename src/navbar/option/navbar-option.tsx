0a19ca463af2e4fa052b98b4d06ceed38fd44bf6
Panth Patel <panthhp@outlook.com>
2023-05-31 21:58:46 +0100

    make open link in new tab option available for this component

--- src/navbar/option/navbar-option.tsx
+++ src/navbar/option/navbar-option.tsx
@@ -1,4 +1,3 @@
-import {useNavigate} from 'react-router-dom';
 import './navbar-option.css'
 
 interface NavbarOptionProps {
@@ -9,16 +8,11 @@ interface NavbarOptionProps {
 }
 
 function NavbarOption(props: NavbarOptionProps) {
-    const navigate = useNavigate()
-
-    function navigateToPath() {
-        navigate(props.path);
-        props.onSelect()
-    }
-
     return (
-        <li className="navbar-option" onClick={navigateToPath} data-selected={props.selected.toString()}>
-            <h1>{props.title}</h1>
+        <li className="navbar-option" data-selected={props.selected.toString()}>
+            <a href={props.path} rel="noreferrer" target="_self" style={{textDecoration: "none", width: "100%", display: "flex", justifyContent: "center", color: "white"}}>
+                <h1>{props.title}</h1>
+            </a>
         </li>
     );
 }
