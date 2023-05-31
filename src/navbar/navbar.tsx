5382dbd9df6b68bf736adb223c9833e1ede8c69b
Panth Patel <panthhp@outlook.com>
2023-05-31 11:57:17 +0100

    text for if we have no sponsors

--- src/navbar/navbar.tsx
+++ src/navbar/navbar.tsx
@@ -50,24 +50,28 @@ function Navbar() {
                             </div> :
                             <img src="/assets/logo-light.png" alt="GEAR" id="img-only-logo"/>}
                     </div>
-                    <div id="sponsor-place">
-                        <h1 style={{fontSize: "1rem"}}>Proudly sponsored by: </h1>
-                        <Marquee pauseOnHover={true}>
-                            {
-                            sponsors.map((sponsor: any) => (
-                                <a className="sponsor" key={sponsor.id} style={{
-                                    display: "flex",
-                                    alignItems: "center",
-                                    color: "white",
-                                    textDecoration: "none"
-                                }} href={sponsor.website} target="_blank" rel="noreferrer">
-                                    <img src={sponsor.photo} style={{height: "5vh", borderRadius: "50%"}} alt=""/>
-                                    <h1 style={{paddingLeft: "1rem", paddingRight: "1rem"}}>{sponsor.name}</h1>
-                                </a>
-                                ))
-                        }
-                        </Marquee>
-                    </div>
+                    {sponsors.length === 0 ? <div style={{color: "white", display: "flex", alignItems: "center", justifyContent: "center", width: "100%"}}>
+                            <h1>No sponsors yet. Sponsor us and get your brand on our robot</h1>
+                        </div> :
+                        <div id="sponsor-place">
+                            <h1 style={{fontSize: "1rem"}}>Proudly sponsored by: </h1>
+                            <Marquee pauseOnHover={true}>
+                                {
+                                    sponsors.map((sponsor: any) => (
+                                        <a className="sponsor" key={sponsor.id} style={{
+                                            display: "flex",
+                                            alignItems: "center",
+                                            color: "white",
+                                            textDecoration: "none"
+                                        }} href={sponsor.website} target="_blank" rel="noreferrer">
+                                            <img src={sponsor.photo} style={{height: "5vh", borderRadius: "50%"}}
+                                                 alt=""/>
+                                            <h1 style={{paddingLeft: "1rem", paddingRight: "1rem"}}>{sponsor.name}</h1>
+                                        </a>
+                                    ))
+                                }
+                            </Marquee>
+                        </div>}
                     <a href="https://www.qebarnet.co.uk" target="_blank" rel="noreferrer">
                         <img src="/assets/qe-logo.png" alt="QE Barnet" style={{padding: "1rem", height: "15vh"}}/>
                     </a>
@@ -99,15 +103,15 @@ function Navbar() {
                     <div id="small-navbar" style={{display: showDropDown ? "flex" : "none"}}>
                         <NavbarOption onSelect={() => setIndex(0)} path="/" selected={index === 0} title="Home"/>
                         <NavbarOption onSelect={() => setIndex(1)} path="/competitions" selected={index === 1}
-                            title="Competitions"/>
+                                      title="Competitions"/>
                         <NavbarOption onSelect={() => setIndex(2)} path="/meet-the-team" selected={index === 2}
-                            title="Meet the team"/>
+                                      title="Meet the team"/>
                         <NavbarOption onSelect={() => setIndex(3)} path="/contact-us" selected={index === 3}
-                            title="Contact us"/>
+                                      title="Contact us"/>
                         <NavbarOption onSelect={() => setIndex(4)} path="/sponsor-us" selected={index === 4}
-                            title="Sponsor us"/>
+                                      title="Sponsor us"/>
                         <NavbarOption onSelect={() => setIndex(5)} path="/download-app" selected={index === 5}
-                            title="20785C app"/>
+                                      title="20785C app"/>
                     </div>
                 </div>
             </nav>
