fefcf1beca2a2278577f7471c87f1f4b265fe953
Panth Patel <panthhp@outlook.com>
2023-06-01 20:15:43 +0100

    add prefilled email to vexoverride@gmail.com for registering sponsorship interest

--- src/sponsor-us/sponsor-us.tsx
+++ src/sponsor-us/sponsor-us.tsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react'
 
 function SponsorUs() {
     const [sponsors, setSponsors] = useState([]) // Save to page state to not lose information when page rerendered
+    const [companyName, setCompanyName] = useState("")
+    const [companyDetails, setCompanyDetails] = useState("")
 
     function loadSponsors() {
         fetch("/data/sponsors.json").then(sponsors => {
@@ -37,6 +39,14 @@ function SponsorUs() {
                 </div>
             }
             <h1>To sponsor us:</h1>
+            <p>Contact us at vexoverride@gmail.com</p>
+            <input className="sponsor-us-input" value={companyName} onChange={(e) => setCompanyName(e.target.value)} placeholder="Enter company name" data-smaller/>
+            <textarea className="sponsor-us-input" value={companyDetails} onChange={(e) => setCompanyDetails(e.target.value)} placeholder="Your terms for sponsorship" data-larger/>
+            <div id="register-sponsorship" onClick={() => {
+                window.open(`mailto:vexoverride@gmail.com?body=Company%3A%20${encodeURIComponent(companyName)}%0d%0aTerms%20for%20sponsorship%3A%20${encodeURIComponent(companyDetails)}&subject=Sponsorship%20interest`)
+            }}>
+                <h1>Register interest now!</h1>
+            </div>
         </div>
     )
 }
