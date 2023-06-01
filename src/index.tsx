91b72753d0ea76be37d94edacc83dbb6983deaaf
Panth Patel <panthhp@outlook.com>
2023-06-01 19:24:19 +0100

    remove redundant global state for listening to route change as can be done with useLocation and useEffect

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./app/app";
 import './index.css';
-import {GlobalState} from "./global-state/global-state";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <GlobalState>
-            <App/>
-        </GlobalState>
+        <App />
     </React.StrictMode>
 );
