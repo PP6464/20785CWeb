d46ed88d6d3f51a2ad9d8631ca577235d9b382ce
Panth Patel <panthhp@outlook.com>
2023-06-02 21:52:24 +0100

    home page

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,9 +1,18 @@
 import './home.css'
+import ReactPlayer from "react-player";
+import {useEffect, useState} from 'react';
 
 function Home() {
+    const [videoURLs, setVideoURLs] = useState([])
+
+    function loadVideos() {
+
+    }
+
     return (
         <div>
             <h1>Home</h1>
+            <ReactPlayer url="https://www.youtube.com/embed/t__G0TSCBdw" controls={true}/>
         </div>
     )
 }
