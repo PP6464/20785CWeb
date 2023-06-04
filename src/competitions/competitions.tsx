6920125270516a4e429f2205d805e2cee57f5bbb
Panth Patel <panthhp@outlook.com>
2023-06-04 12:25:24 +0100

    start adding functionality to load events

--- src/competitions/competitions.tsx
+++ src/competitions/competitions.tsx
@@ -1,6 +1,14 @@
+import { useState } from 'react'
 import './competitions.css'
 
+interface Event {
+
+}
+
 function Competitions() {
+    const [events, loadEvents] = useState([])
+
+
     return (
         <div>
             <h1>Competitions</h1>
