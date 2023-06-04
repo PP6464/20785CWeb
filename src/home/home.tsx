af646f86324131d6301546af2ee6b06207e86cbc
Panth Patel <panthhp@outlook.com>
2023-06-04 12:25:45 +0100

    experiment with width and height of react player

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -12,7 +12,7 @@ function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <ReactPlayer url="https://www.youtube.com/embed/t__G0TSCBdw" controls={true}/>
+            <ReactPlayer url="https://www.youtube.com/embed/t__G0TSCBdw" controls={true} width="50vw" height="50vh"/>
         </div>
     )
 }
