0ea6aaa878f5fd9bca36133d69e1f63aa8865822
Panth Patel <panthhp@outlook.com>
2023-06-06 19:50:34 +0100

    add hard max size limit of 100px

--- src/loading/loading.tsx
+++ src/loading/loading.tsx
@@ -8,9 +8,9 @@ interface LoadingProps {
 function Loading(props: LoadingProps) {
   return (
     <div id="loading-container">
-      <h1 style={{fontSize: props.size, color: props.color}}>2</h1>
-      <img style={{height: props.size}} src={props.color === "black" ? "/assets/logo.png" : "/assets/logo-light.png"} alt="GEAR" />
-      <h1 style={{fontSize: props.size, color: props.color}}>785C</h1>
+      <h1 style={{fontSize: `min(${props.size}, 100px)`, color: props.color}}>2</h1>
+      <img style={{height: `min(${props.size}, 100px)`}} src={props.color === "black" ? "/assets/logo.png" : "/assets/logo-light.png"} alt="GEAR" />
+      <h1 style={{fontSize: `min(${props.size}, 100px)`, color: props.color}}>785C</h1>
     </div>
   )
 }
