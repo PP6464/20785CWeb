664fdfa9cce4cf57f8af192026cea8d0fd5ec8dd
Panth Patel <panthhp@outlook.com>
2023-06-06 20:48:14 +0100

    connect to firebase

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app'
+import firebase from "firebase/compat";
+
+initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+})
+
+export const firestore = firebase.firestore()
