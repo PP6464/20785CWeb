fcec66117ad8648fcaeffb3c811b95dd75fc7732
Panth Patel <panthhp@outlook.com>
2023-06-06 20:59:04 +0100

    load feeds from firebase

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,18 +1,43 @@
 import './home.css'
-import ReactPlayer from "react-player"
 import {useEffect, useState} from 'react'
+import { firestore } from '../firebase/firebase'
+
+type Feed = {
+    title: string,
+    value: string,
+    type: string
+}
 
 function Home() {
-    const [videoURLs, setVideoURLs] = useState([])
+    const [feeds, setFeeds] = useState<Feed[]>([]) // Feeds for the home page
+    const [category, setCategory] = useState("all") // Whether text, video or all feeds
+    const [loading, setLoading] = useState(false)
 
-    function loadVideos() {
-
+    function loadFeeds() {
+        setLoading(true) // Show loading animation
+        firestore.collection("feeds").where("type", "!=", category === "text" ? "video" : category === "video" ? "text" : "").onSnapshot((snapshot) => {
+            setFeeds(snapshot.docs.map((doc) => {
+                return {
+                    title: doc.data().title,
+                    value: doc.data().value,
+                    type: doc.data().type,
+                }
+            }))
+        }) // Load feeds from firestore
+        setLoading(false) // Hide loading animation
     }
+
+    useEffect(loadFeeds, [category])
 
     return (
         <div>
             <h1>Home</h1>
-            <ReactPlayer url="https://www.youtube.com/embed/t__G0TSCBdw" controls={true} width="50vw" height="50vh"/>
+            <select onChange={(e) => { setCategory(e.target.value) }}>
+                <option value="all">All</option>
+                <option value="video">Video</option>
+                <option value="text">Text</option>
+            </select>
+
         </div>
     )
 }
