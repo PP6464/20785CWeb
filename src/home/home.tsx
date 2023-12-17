8c681733f41274ae485f5d8e92ebeb5045d514d9
Laziness <78thunderstorm@gmail.com>
2023-12-17 20:49:24 +0000

    gofundme

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,29 +1,21 @@
+import React from "react";
 import "./home.css";
 
 function Home() {
   return (
     <div id="home-container">
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          width: "100%",
-          padding: "0 10px",
-        }}
-      >
-        <h1 style={{ textDecoration: "underline", fontSize: "40px" }}>
-          Home
-        </h1>
-        <div className="home-card">
-          <div style={{display: "flex", alignItems: "center", justifyContent: "center", width: "100%", position: "relative"}}>
-            <h1>Design Award: QE Competition</h1>
-            <p style={{justifyContent: "end", position: "absolute", right: "0"}}>09/12/2023</p>
-          </div>
-          <img src="/assets/award.jpg" alt="" width="500px"/>
-          <p>We won the design award at the QE competition! 🏆</p>
+      <h1 style={{ textDecoration: "underline", fontSize: "40px" }}>Home</h1>
+      <div className="home-card" style={{ display: "flex", flexDirection: "column" }}>
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", width: "100%", position: "relative" }}>
+          <h1>Design Award: QE Competition</h1>
+          <p style={{ textAlign: "end", position: "absolute", right: "0", margin: "0" }}>09/12/2023</p>
         </div>
+        <div style={{ display: "flex" }}>
+          <img src="/assets/award.jpg" alt="" style={{ width: "100%", maxWidth: "500px" }} />
+        </div>
+        <p>We won the design award at the QE competition! 🏆</p>
       </div>
+      <embed height="500px" src="https://www.gofundme.com/f/help-override-20785c-compete-in-the-2324-season" type="text/html"></embed>
     </div>
   );
 }
